533413b545defcd0a4fe90ec68f64067c0a32544
Héctor Vásquez <70999302+HectorVR-Dev@users.noreply.github.com>
2023-10-13 09:47:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
 import pandas as pd
 from PokeWinner import *
-from pokemodelo import inferencia
 
 # Cargar la base de datos de Pokémon desde un archivo CSV
 name_dateset = "ProyectoFinal//dataset_pokemons.csv"
 df_pokemon = pd.read_csv(name_dateset)
 
 # Especificar el ID del Pokémon que deseas obtener
-id_pokemon_deseado = 25#int(input('Ingrese ID de Pokedex:')) # Por ejemplo, Pikachu es 25
-
+id_pokemon_deseado = 12 #int(input('Ingrese ID de Pokedex:')) # Por ejemplo, Pikachu es 25
+n_candidatos = 10 #int(input('Ingrese ID de Pokedex:'))
 # Comprobar si se encontró el Pokémon
 poke_objetivo = df_pokemon[df_pokemon['ID'] == id_pokemon_deseado].values.tolist()[0]
 
 
+print_poke(poke_objetivo)
 
-print(f"El pokemon a derrotar es {poke_objetivo[1]}")
 filtrado_logico = poke_logic()
 tipos = filtrado_logico.tipos_winner(poke_objetivo)
-print(f'El PokeWinner debe ser de almenos los siguientes tipos:\n\n{tipos[:-1]}\n')
+print(f'\nEl PokeWinner debe ser de almenos uno los siguientes tipos: \n{tipos[:-1]}\n')
 
 poke_winner = poke_winner(name_dateset)
-candidatos = poke_winner.buscar(poke_objetivo, tipos)
-cont = 1
+candidatos = poke_winner.buscar(poke_objetivo, tipos, n_candidatos)
+
+
+columnas = ['ID', 'Nombre', 'Tipo1', 'Tipo2']
+df_temp = pd.DataFrame(columns=columnas)
 for ID in candidatos:
     PokeWinner = df_pokemon[df_pokemon['ID'] == ID].values.tolist()[0]
-    print(f'{cont}. {PokeWinner[1]} de tipo {PokeWinner[3]} y {PokeWinner[4]}')
-    cont+= 1
+    nueva_fila = {'ID': PokeWinner[0], 'Nombre': PokeWinner[1], 'Tipo1': PokeWinner[3], 'Tipo2': PokeWinner[4]}
+    df_temp = df_temp._append(nueva_fila, ignore_index=True)
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+print(df_temp)
+
+P_candidatos = inferencia(poke_winner.com_est(poke_objetivo, candidatos))
+best = mejores(candidatos, P_candidatos, 3)
 print()
-pro = poke_winner.com_est(poke_objetivo, candidatos)
-
-probabilidades = inferencia(pro)
-
-# Obtiene la ruta que tiene la mayor probabilidad de llegar a tiempo
-PokeWinner_WIN = [df_pokemon[df_pokemon['ID'] == candidatos[probabilidades.index(max(probabilidades))]].values.tolist()[0][1] , max(probabilidades)]
-#print(probabilidades)
-print(f"El PokeWinner elegido es {PokeWinner_WIN[0]} con una probabilidad de {PokeWinner_WIN[1]}")
-
-
+for opciones in best:
+    PokeWinner_WIN = [df_pokemon[df_pokemon['ID'] == int(opciones[0])].values.tolist()[0][1] , float(opciones[1])]
+    print(f"El PokeWinner elegido es {PokeWinner_WIN[0]} con una probabilidad de {PokeWinner_WIN[1]}")
